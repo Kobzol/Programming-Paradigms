386ad77e607467fef80cd15ae4b65e5df1ff5c17
Jakub Beránek <berykubik@gmail.com>
2016-09-28 10:20:01 +0200

    ENH: added basis of homework 3

---
+++ cviko3.hs
@@ -0,0 +1,8 @@
+module Cviko3 where
+
+import Prelude
+
+-- pattern matching
+-- fn st = case (st) of
+--         [] -> '0'
+--         (x:_) -> x
---
+++ du3.hs
@@ -0,0 +1,126 @@
+module Du3 where
+
+import Prelude
+import Control.Exception.Base
+import Debug.Trace
+import Data.Array
+import Data.Ix
+
+maxLineLength = 30
+vstup = "In computer science, \tfunctional programming is a programming \tparadigm that treats computation   as \tthe evaluation of mathematical functions and avoids state and mutable data.    It emphasizes the application of functions, in contrast to the imperative \tprogramming style, which emphasizes changes in state."
+--vstup = "aaa bb cc ddddd"
+
+-- dynamické programování - rozdelit na co nejmenší pocet rádku
+
+repeatItem :: a -> Int -> [a]
+repeatItem a count = take count (repeat a)
+
+lineLength :: [String] -> Int -> Int -> Int
+lineLength line spaceWidth maxLength =
+  if length line == 0 then 0
+  else startLength + appendLength
+  where
+    wordLength = sum (map (length) line)
+    safeWords = init line
+    safeWordLength = spaceWidth * (length safeWords)
+    startLength = wordLength + safeWordLength
+    appendLength = max 0 (min (maxLength - (startLength)) spaceWidth)
+
+widenLine :: [String] -> Int -> Int -> String
+widenLine line spaceWidth maxLength = 
+  if widenedLength < maxLength then widenLine line (spaceWidth + 1) maxLength
+  else if widenedLength > maxLength then makeLine line (spaceWidth - 1) maxLength
+  else makeLine line spaceWidth maxLength
+  where
+    widenedLength = lineLength line spaceWidth maxLength
+
+makeLine :: [String] -> Int -> Int -> String
+makeLine line spaceWidth maxLength = take maxLength joined
+  where
+    joined = concat (map (++ (repeatItem ' ' spaceWidth)) line)
+
+lineify :: [String] -> String
+lineify s = concat (map (++ "\n") s)
+
+
+greedyJustify :: [String] -> [String] -> [String] -> Int -> [String]
+greedyJustify [] line res maxLength = res ++ [(makeLine line 1 maxLength)]
+greedyJustify (word:ls) line res maxLength =
+  if appendedLength < maxLength then greedyJustify ls appended res maxLength
+  else if appendedLength > maxLength then greedyJustify (word:ls) [] (res ++ [widenedLine]) maxLength
+  else greedyJustify ls [] (res ++ [widenedAppended]) maxLength
+  where
+    appended = line ++ [word]
+    appendedLength = lineLength appended 1 maxLength
+    widenedLine = widenLine line 1 maxLength
+    widenedAppended = widenLine appended 1 maxLength
+
+
+splitWordAt :: String -> Int -> (String, String)
+splitWordAt s i = ((take i s), (drop i s))
+
+hyphenateJustify :: [String] -> [String] -> [String] -> Int -> [String]
+hyphenateJustify [] line res maxLength = res ++ [(makeLine line 1 maxLength)]
+hyphenateJustify (word:ls) line res maxLength =
+  if appendedLength < maxLength then hyphenateJustify ls appended res maxLength
+  else if (appendedLength > maxLength) && (length leftHalf > 0) then hyphenateJustify (rightHalf:ls) [] (res ++ [widenedLineHyphen]) maxLength
+  else if appendedLength > maxLength then hyphenateJustify (word:ls) [] (res ++ [widenedLine]) maxLength
+  else hyphenateJustify ls [] (res ++ [widenedAppended]) maxLength
+  where
+    appended = line ++ [word]
+    appendedLength = lineLength appended 1 maxLength
+    widenedLine = widenLine line 1 maxLength
+    widenedLineHyphen = widenLine (line ++ [leftHalf ++ "-"]) 1 maxLength
+    widenedAppended = widenLine appended 1 maxLength
+    (leftHalf, rightHalf) = splitWordAt word (((length word) - (appendedLength - maxLength)) - 1)
+
+
+backtrackJustify :: [String] -> [String] -> [String] -> Int -> [String]
+backtrackJustify [] line res maxLength = res ++ [(makeLine line 1 maxLength)]
+backtrackJustify (word:ls) line res maxLength =
+  if (appendedLength < maxLength) && (length line > 0) && (maxLength - appendedLength < (10)) then
+    if scoreKeep < scoreMove then linesKeep
+    else linesMove
+  else if appendedLength < maxLength then linesKeep
+  else if appendedLength > maxLength then linesMove
+  else greedyJustify ls [] (res ++ [widenedAppended]) maxLength
+  where
+    appended = line ++ [word]
+    appendedLength = lineLength appended 1 maxLength
+    widenedLine = widenLine line 1 maxLength
+    widenedAppended = widenLine appended 1 maxLength
+    linesKeep = backtrackJustify ls appended res maxLength
+    linesMove = backtrackJustify (word:ls) [] (res ++ [widenedLine]) maxLength
+    scoreKeep = scoreLines (linesKeep)
+    scoreMove = scoreLines (linesMove)
+
+
+scoreLines :: [String] -> Int
+scoreLines l = sum (map (\x -> scoreSquare (scoreLine x)) l)
+  where
+    scoreSquare x = x * x
+    scoreLine l = length (takeWhile (\x -> x == ' ') (reverse l))
+
+
+badness :: [String] -> Int -> Int -> Int -> Int
+badness w i j maxLength = badnessScore (take (j-i) (drop i w))
+  where
+    badnessScore w = if len > maxLength then 1000
+                     else left * left
+      where
+        len = lineLength w 1 maxLength
+        left = maxLength - len
+
+memoD :: [String] -> Int -> Int
+memoD w maxLength = innerMemo 0
+  where
+    innerMemo i =
+      if i == len then 0
+      else 1--minimum [(badness w i j) + memos ! j | j <- [len, (len-1)..(i + 1)]]
+    memos = listArray (0, len) [innerMemo i | i <- [0..len]]
+    len = length w
+
+
+du :: ([String] -> [String] -> [String] -> Int -> [String]) -> IO ()
+du x = putStr (lineify (duRaw x))
+duRaw x = x (words vstup) [] [] maxLineLength
